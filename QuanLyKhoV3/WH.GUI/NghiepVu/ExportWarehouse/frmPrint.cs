5b81722a67f9b8ee3b73f2b2ee71039d1004b8a5
Pham Duc Thien <thien.pd@mekongpetro.com>
2022-10-27 22:19:52 +0700

    save

--- QuanLyKhoV3/WH.GUI/NghiepVu/ExportWarehouse/frmPrint.cs
+++ QuanLyKhoV3/WH.GUI/NghiepVu/ExportWarehouse/frmPrint.cs
@@ -1,20 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Reporting.WinForms;
+using WH.GUI.Dto;
 
-namespace WH.GUI.NghiepVu.ExportWarehouse
+namespace WH.GUI.ExportWarehouse
 {
     public partial class frmPrint : Form
     {
-        public frmPrint()
+        private readonly Receipt _receipt;
+        private List<ReceiptItem> _receiptItem;
+
+        public frmPrint(Receipt receipt, List<ReceiptItem> receiptItems)
         {
             InitializeComponent();
+            _receipt = receipt;
+            _receiptItem = receiptItems;
+        }
+
+        private void frmPrint_Load(object sender, EventArgs e)
+        {
+            var para = new[]
+            {
+                new ReportParameter("pCustomerName", _receipt.CustomerName),
+                new ReportParameter("pPhone", _receipt.Phone),
+                new ReportParameter("pAddress", _receipt.Address),
+                new ReportParameter("pDate", DateTime.Now.ToShortDateString()),
+            };
+            reportViewer1.LocalReport.SetParameters(para);
+            this.reportViewer1.RefreshReport();
+            this.reportViewer1.RefreshReport();
         }
     }
 }
