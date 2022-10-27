3d3bb3813a709711d026f88bb1091683aadff00f
Pham Duc Thien <thien.pd@mekongpetro.com>
2022-10-27 21:34:46 +0700

    save

---
+++ QuanLyKhoV3/WH.GUI/Dto/Receipt.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace WH.GUI.Dto
+{
+    public class Receipt
+    {
+        public string CustomerName { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string TotalAmount { get; set; }
+        public string Payment { get; set; }
+        public string Description { get; set; }
+
+        public List<ReceiptItem> ReceiptItems { get; set; }
+    }
+
+    public class ReceiptItem
+    {
+        public string Stt { get; set; }
+        public string ProductCode { get; set; }
+        public string ProductName { get; set; }
+        public string Units { get; set; }
+        public string Discount { get; set; }
+        public string Price { get; set; }
+        public string Amount { get; set; }
+    }
+}
